0454650c1e76d286be25e1fbbdd4991a1b2209a7
Eder Henrique Biason de Oliveira <ederbiason@hotmail.com>
2023-11-09 21:32:32 -0300

    feat: finish login page and add signup page

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,108 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import { Form, FormField, FormItem, FormControl, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import Link from "next/link"
+import { GoogleSignInButton } from "@/components/GoogleSignInButton"
+import { Chrome } from "lucide-react"
+
+const FormSchema = z.object({
+    name: z.string().min(1, "O nome é obrigatório!"),
+    email: z.string().min(1, "O email é obrigatório!").email("Email inválido."),
+    password: z.string().min(1, "A senha é obrigatória!").min(8, "A senha deve ter no minímo 8 caracteres."),
+    remember: z.boolean().default(false).optional(),
+})
+
+export function SignUpForm() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            remember: false
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof FormSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-gray-900 font-semibold text-3xl mt-7">
+                Crie uma conta
+            </h1>
+
+            <p className="text-gray-500">
+                Seja bem-vindo! Informe seus dados
+            </p>
+
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nome*</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="Insira o seu nome" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email*</FormLabel>
+                                <FormControl>
+                                    <Input type="email" placeholder="Insira o seu email" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Senha*</FormLabel>
+                                <FormControl>
+                                    <Input type="password" placeholder="Insira sua senha" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button className="w-full bg-blue-600 hover:bg-blue-800 font-semibold text-md" type="submit">Cadastrar</Button>
+                </form>
+
+                <GoogleSignInButton>
+                    <Chrome />
+                    Cadastrar com o Google
+                </GoogleSignInButton>
+
+                <div className="flex gap-2">
+                    <p className="text-gray-500">Já possui uma conta?</p>
+
+                    <Link href="/login" className="text-blue-600 hover:underline">
+                        Entrar
+                    </Link>
+                </div>
+            </Form>
+        </div>
+    )
+}
