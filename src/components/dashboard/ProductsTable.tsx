992a16b6ab07c75201c02cb59116cd8388f10dff
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-11-14 15:38:35 -0300

    feat: finish products page

--- src/components/dashboard/ProductsTable.tsx
+++ src/components/dashboard/ProductsTable.tsx
@@ -6,13 +6,145 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
+} from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { ListFilter } from "lucide-react"
+import { ProductForm } from "@/components/form/ProductForm"
 
 export function ProductsTable() {
     return (
         <div className="mt-14">
+            <div className="flex items-center justify-between px-4 pt-5 pb-2">
+                <h1 className="text-gray-800 text-xl font-semibold">
+                    Produtos
+                </h1>
+
+                <div className="flex items-center justify-center gap-4">
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button className="bg-blue-600 hover:bg-blue-800">
+                                Adicionar produto
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Novo produto</DialogTitle>
+                            </DialogHeader>
+                            <div className="grid gap-4 py-4 ">
+                                <ProductForm />
+
+                                {/* <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Nome do produto
+                                    </Label>
+                                    <Input
+                                        id="name"
+                                        className="col-span-3"
+                                        placeholder="Insira o nome do produto"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="id" className="text-right">
+                                        ID do produto
+                                    </Label>
+                                    <Input
+                                        id="id"
+                                        className="col-span-3"
+                                        placeholder="Insira o ID do produto"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="category" className="text-right">
+                                        Categoria
+                                    </Label>
+                                    <Input
+                                        id="category"
+                                        className="col-span-3"
+                                        placeholder="Selecione a categoria"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="price" className="text-right">
+                                        Preço de compra
+                                    </Label>
+                                    <Input
+                                        id="price"
+                                        className="col-span-3"
+                                        placeholder="Insira o preço de compra"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="quantity" className="text-right">
+                                        Quantidade
+                                    </Label>
+                                    <Input
+                                        id="quantity"
+                                        className="col-span-3"
+                                        placeholder="Insira a quantidade"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="validity" className="text-right">
+                                        Validade
+                                    </Label>
+                                    <Input
+                                        id="validity"
+                                        className="col-span-3"
+                                        placeholder="Insira a validade"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="minthreshold" className="text-right">
+                                        Qtd. mínima
+                                    </Label>
+                                    <Input
+                                        id="minthreshold"
+                                        className="col-span-3"
+                                        placeholder="Insira a qtd. mínima"
+                                    />
+                                </div> */}
+                            </div>
+                            <DialogFooter>
+                                <DialogClose>
+                                    <Button className="border border-zinc-400 bg-white hover:bg-zinc-200 text-gray-400">
+                                        Cancelar
+                                    </Button>
+                                </DialogClose>
+                                
+                                <Button 
+                                    type="submit"
+                                    className="bg-blue-600 hover:bg-blue-800"
+                                >
+                                    Adicionar
+                                </Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+
+                    <Button className="border border-zinc-400 bg-white hover:bg-zinc-200 text-gray-600">
+                        <ListFilter />
+                        Filtros
+                    </Button>
+
+                    <Button className="border border-zinc-400 bg-white hover:bg-zinc-200 text-gray-600">
+                        Baixar tudo
+                    </Button>
+                </div>
+            </div>
+
             <Table className="">
-                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableCaption>Lista dos produtos</TableCaption>
                 <TableHeader>
                     <TableRow>
                         <TableHead>Produto</TableHead>
@@ -64,7 +196,7 @@ export function ProductsTable() {
                         <TableCell>**/**/****</TableCell>
                         <TableCell className="text-green-500">Disponível</TableCell>
                     </TableRow>
-                    
+
                 </TableBody>
             </Table>
 
