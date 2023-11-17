d35d93d848ee00a4aff24e00df49b8f66876792f
Eder Henrique Biason de Oliveira <ederbiason@hotmail.com>
2023-11-17 19:47:15 -0300

    feat: add orders table

---
+++ src/app/(dashboard)/orders/page.tsx
@@ -0,0 +1,9 @@
+import { OrdersTable } from "@/components/dashboard/OrdersTable";
+
+export default function page() {
+  return (
+    <div className="w-full">
+      <OrdersTable />
+    </div>
+  )
+}
