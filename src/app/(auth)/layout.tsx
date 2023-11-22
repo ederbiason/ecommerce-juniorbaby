9250b968266e03d7feaa6f4e4982411ce8eadd73
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2023-11-22 12:29:26 -0300

    feat: protecting pages with middleware

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,13 +3,13 @@ import logoImage from '../../../public/logo.png'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full px-28 mt-10 items-center justify-center">
+    <div className="flex w-full px-28 items-center justify-center">
         <Image 
           src={logoImage}
           alt="Logo image"
         />
 
-        <div className="w-full flex flex-col">
+        <div className="w-2/4 flex flex-col">
           {children}
         </div>
     </div>
---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    let isPublicRoute = false 
+
+    if(request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/signup") {
+        isPublicRoute = true
+    }
+
+    // Se não tiver um token e a rota for privada, redireciona para o login
+    const token = request.cookies.get("token")?.value || ""
+
+    if(!token && !isPublicRoute) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    // Se tiver token e rota for publica, redireciona para a home
+    if(token && isPublicRoute) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/login', '/signup', '/'],
+}
