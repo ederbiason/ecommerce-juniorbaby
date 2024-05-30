68b7026c56b5d826677fdb1d6d19dca6fc72b588
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2024-05-30 10:05:01 -0300

    fix: respontivity styles

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import logoImage from '../../../public/logo.png'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full px-28 items-center justify-center lg:py-10 2xl:py-20">
+    <div className="flex w-full px-28 items-center justify-center lg:py-10 xl:py-28 2xl:py-52">
         <Image 
           src={logoImage}
           alt="Logo image"
